cc47dd6d1a308900a890074a8c7d156321a035f5
suday95 <sudaynandan95@gmail.com>
2024-10-15 23:44:28 +0530

    Updated navbar styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import logo from './logo.svg';
-import React,{useState} from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Teams from './components/Teams.js';
@@ -7,11 +6,14 @@ import App1 from './App1.js';
 import Home from './components/Home.jsx';
 function App() {
   return (
+    
         <div className="App">
       <Router>
+        
         <Routes>
-          <Route path="/home" element={<App1 />} />
-          <Route path="/teams" element={<Teams />} />
+        <Route path="/KodeinKGP" element={<App1 />} />
+          <Route path="/home" element={<App1 />}/>
+          <Route path="/teams" element={<Teams />}/>
         </Routes>
       </Router>
     </div>
